5bf20928bbe45fb901f5ede7b5fe23bff47f7fcc
Irfan Noor <37165903+IrfanNoor@users.noreply.github.com>
2018-04-02 19:00:46 +0500

    Add files via upload

---
+++ js/logging.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	$.get('http://download.templatemo.com/themes/log?id='+557254+'&oi='+18+'&ot=1&&url='+window.location, function(json){})    
+
+});
---
+++ staff/index.php
@@ -0,0 +1,3 @@
+<?php //Developed by Freelance developer. If you finding any trouble please visit http://www.freestudentprojects.com/phpscript/web-vehicle-agent-system/ for support page.
+header("Location: ../staff_login.php");
+?>
